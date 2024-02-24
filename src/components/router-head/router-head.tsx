4b3859d27ef5de09eabf2027041b0c23f0d0469f
Hugo Alliaume <hugo@alliau.me>
2024-02-24 13:25:10 +0100

    feat: setup Partytown and Google Analytics

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { QwikPartytown } from '../partytown/partytown';
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -27,6 +28,19 @@ export const RouterHead = component$(() => {
       {head.styles.map((s) => (
         <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
+
+      <QwikPartytown forward={['dataLayer.push']} />
+      <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-16T8LV0V8T" />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-16T8LV0V8T');
+      `}
+      ></script>
     </>
   );
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { macroPlugin } from "@builder.io/vite-plugin-macro";
+import { defineConfig } from 'vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { macroPlugin } from '@builder.io/vite-plugin-macro';
+import { partytownVite } from '@builder.io/partytown/utils';
+import { join } from 'path';
 
 export default defineConfig(() => {
   return {
     plugins: [
-      macroPlugin({ preset: "pandacss" }),
+      macroPlugin({ preset: 'pandacss' }),
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
+      partytownVite({ dest: join(__dirname, 'dist', '~partytown') }),
     ],
     dev: {
       headers: {
-        "Cache-Control": "public, max-age=0",
+        'Cache-Control': 'public, max-age=0',
       },
     },
     preview: {
       headers: {
-        "Cache-Control": "public, max-age=600",
+        'Cache-Control': 'public, max-age=600',
       },
     },
   };
